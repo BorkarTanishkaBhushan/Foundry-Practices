349be82f8186ace474dd62e5f9c0c826f861f2ab
tanishkaBBR <tanishka.b@somaiya.edu>
2023-06-21 23:41:57 +0530

    Added SimpleStorage contract files

---
+++ script/SimpleStorage.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function setUp() public {}
+
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
+
+
+
