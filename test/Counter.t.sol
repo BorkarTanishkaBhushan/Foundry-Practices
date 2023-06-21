52e80da5d382a22d7da4052d6d83a00e6fbe6d20
tanishkaBBR <tanishka.b@somaiya.edu>
2023-06-22 00:05:10 +0530

    Modified Counter related files

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -4,21 +4,35 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
 
+import "forge-std/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 contract CounterTest is Test {
     Counter public counter;
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testInc() public {
+        counter.inc();
+
+        assertEq(counter.count(), 1);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testFailDec() public {
+        counter.dec();
+    }
+
+    function testDecUnderflow() public {
+        vm.expectRevert(stdError.arithmeticError);
+        counter.dec();
+    }
+
+    function testDec() public {
+        counter.inc();
+        counter.inc();
+        counter.dec();
+        assertEq(counter.count(), 1);
     }
 }
