6e11f2be76e29396e9cb836c2e713c656baa7782
tanishkaBBR <tanishka.b@somaiya.edu>
2023-06-23 17:10:15 +0530

    Invariant testing

--- test/Invariant2.t.sol
+++ test/Invariant2.t.sol
@@ -5,10 +5,7 @@ import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import {WETH} from "../src/WETH.sol";
 
-// https://book.getfoundry.sh/forge/invariant-testing?highlight=targetSelector#invariant-targets
-// https://mirror.xyz/horsefacts.eth/Jex2YVaO65dda6zEyfM_-DXlXhOWCAoSpOx5PLocYgw
 
-// NOTE: open testing - randomly call all public functions
 contract WETH_Open_Invariant_Tests is Test {
     WETH public weth;
 
